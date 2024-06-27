e5e48891e3472634ee718ac937039573939618a3
Atishey jain <atisheyjain1232@gmail.com>
2024-06-27 13:14:56 +0530

    fix CORS

--- app.js
+++ app.js
@@ -34,9 +34,9 @@ app.enable("trust proxy");
 
 // 1) GLOBAL MIDDLEWARES
 
-// app.use(cors({ credentials: true, origin: "https://inqueue-2f51e.web.app" }));
+app.use(cors({ credentials: true, origin: "https://inqueue-2f51e.web.app" }));
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+// app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 // Access-Control-Allow-Origin *
 
 app.options("*", cors());
